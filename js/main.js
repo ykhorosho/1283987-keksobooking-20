6186f6e6ff79cc66bc16fd14298dc14f54574c08
Yana <vsebydetxoposho@gmail.ru>
2020-06-08 16:22:54 +0300

    zadanye

--- js/main.js
+++ js/main.js
@@ -1,52 +1,105 @@
 'use strict';
 
-var OBJECTS = 8;
-var TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var CHECKINS = ['12:00', '13:00', '14:00'];
-var CHECKOUT = ['12:00', '13:00', '14:00'];
-var PHOTOS = [
+var OFFERS_COUNT = 8;
+var offersTypes = ['palace', 'flat', 'house', 'bungalo'];
+var offersChekins = ['12:00', '13:00', '14:00'];
+var offersCheckout = ['12:00', '13:00', '14:00'];
+var offersPhotos = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
-var FEATUERES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var offersTitles = ['Предложение отличное', 'Плохое предложение'];
+var offersAddress = ['600, 350', '500, 250', '400, 150'];
+var offersPrice = [10, 300, 1000];
+var offersRooms = [1, 2, 3, 4, 5];
+var offersGuests = [1, 2, 3, 4, 5];
+var offersFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var offersDescription = ['Большая комната', 'Маленькая комната'];
 
+var OFFSET_X = 25;
+var OFFSET_Y = 70;
 
-var getRandomArray = function (FEATUERES) {
-  for (var i = 0; i < FEATUERES.length; i++){
-
-  }
-
-
-  for (var i = 0; i < OBJECTS; i++) {
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
-{
-  'author': {
-    'avatar': 'img/avatars/user0' + i + '.png',
-  },
-  'offer': {
-      'title': 'строка, заголовок предложения',
-      'address': 'строка, адрес предложения. Для простоты пусть пока представляет собой запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
-      'price': число, стоимость
-      'type': строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
-      'rooms': число, количество комнат
-      'guests': число, количество гостей, которое можно разместить
-      'checkin': getRandom,
-      'checkout': getRandom,
-      'features': getRandomArray(FEATUERES.length),
-      'description': строка с описанием,
-      'photos': массив строк случайной длины, содержащий адреса фотографий "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-  },
-  'location': {
-      'x': getRandomArray(0, 630)случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
-      'y': getRandomArray(130, 630)
-  }
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+var createOffer = function (objectCount) {
+  var featuresArrayLength = getRandomIntInclusive(1, offersFeatures.length);
+  var featuresArray = [];
+  for (var i = 0; i < featuresArrayLength; i++) {
+    featuresArray.push(offersFeatures[i]);
+  }
+  var photosArrayLength = getRandomIntInclusive(1, offersPhotos.length);
+  var photosArray = [];
+  for (var k = 0; k < photosArrayLength; k++) {
+    photosArray.push(offersPhotos[k]);
+  }
+  return {
+    'author': {
+      'avatar': 'img/avatars/user0' + ++objectCount + '.png',
+    },
+    'offer': {
+      'title': offersTitles[getRandomInt(offersTitles.length)],
+      'address': offersAddress[getRandomInt(offersAddress.length)],
+      'price': offersPrice[getRandomInt(offersPrice.length)],
+      'type': offersTypes[getRandomInt(offersTypes.length)],
+      'rooms': offersRooms[getRandomInt(offersRooms.length)],
+      'guests': offersGuests[getRandomInt(offersGuests.length)],
+      'checkin': offersChekins[getRandomInt(offersChekins.length)],
+      'checkout': offersCheckout[getRandomInt(offersCheckout.length)],
+      'features': featuresArray,
+      'description': offersDescription[getRandomInt(offersDescription.length)],
+      'photos': photosArray
+    },
+    'location': {
+      'x': getRandomIntInclusive(50, 930),
+      'y': getRandomIntInclusive(130, 630)
+    }
+  };
+};
+
+var createOffers = function () {
+  var offers = [];
+  for (var i = 0; i < OFFERS_COUNT; i++) {
+    offers.push(createOffer(i));
+  }
+  return offers;
+};
+
+var createdOffers = createOffers();
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
+
 var mapPinsBlock = document.querySelector('.map__pins');
-var mapPin = document.querySelector('#pin')
-  .content
-  .querySelector('.map__pin');
+
+var mapPin = document.querySelector('#pin');
+
+var createOfferElement = function (offerObject, pinElement) {
+  var mapPinElement = pinElement.content.cloneNode(true);
+  var mapPinButton = mapPinElement.querySelector('.map__pin');
+  mapPinButton.style.cssText = 'left: ' + (offerObject.location.x - OFFSET_X) + 'px; top: ' + (offerObject.location.y - OFFSET_Y) + 'px;';
+  var mapPinImage = mapPinButton.querySelector('img');
+  mapPinImage.src = offerObject.author.avatar;
+  mapPinImage.alt = offerObject.offer.title;
+  return mapPinElement;
+};
+
+
+var appendOfferElements = function (createOfferElementFunction, offers, mapPinsElement, mapPinElement) {
+
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < offers.length; i++) {
+    var currentOffer = createOfferElementFunction(offers[i], mapPinElement);
+    fragment.appendChild(currentOffer);
+  }
+  mapPinsElement.appendChild(fragment);
+};
+
+appendOfferElements(createOfferElement, createdOffers, mapPinsBlock, mapPin);
