8580ee4bee72f649821beed80d53c5600ef03f89
Yana <vsebydetxoposho@gmail.ru>
2020-07-30 00:42:22 +0800

    устранение дребезга

--- js/debounce.js
+++ js/debounce.js
@@ -1,18 +1,18 @@
 'use strict';
- (function () {
-   var DEBOUNCE_INTERVAL = 500; //ms
+(function () {
+  var DEBOUNCE_INTERVAL = 500; //ms
 
-   window.debounce = function (cb) {
-     var lastTimeout = null;
+  window.debounce = function (cb) {
+    var lastTimeout = null;
 
-     return function () {
-       var parameters = arguments;
-       if (lastTimeout) {
-         window.clearTimeout(lastTimeout);
-       }
-       lastTimeout = window.setTimeout(function () {
-         cb.apply(null, parameters);
-       }, DEBOUNCE_INTERVAL);
-     };
-   };
- })();
+    return function () {
+      var parameters = arguments;
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(function () {
+        cb.apply(null, parameters);
+      }, DEBOUNCE_INTERVAL);
+    };
+  };
+})();
